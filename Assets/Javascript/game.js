7153fe2c15f3ddaed71edbaa9d34c9efd3aeada6
Raymond _ <raymondt1234@gmail.com>
2019-09-26 15:24:32 -0700

    Working on allowing the player to select an enemy to fight

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,15 +1,13 @@
-// Classes
-class fighter {
-    constructor(name, healthPoints, attackPower, counterAttackPower) {
-        this.name = name;
-        this.healthPoints = healthPoints;
-        this.attackPower = attackPower;
-        this.counterAttackPower = counterAttackPower;
-    }
-    toString() {
+$(document).ready(function () {
+function fighter(name, healthPoints, attackPower, counterAttackPower) {
+    this.name = name;
+    this.healthPoints = healthPoints;
+    this.attackPower = attackPower;
+    this.counterAttackPower = counterAttackPower;
+    this.toString = function () {
         return `Name: ${this.name} HP: ${this.healthPoints} AP: ${this.attackPower} CAP: ${this.counterAttackPower}`;
     }
-}
+};
 // Functions
 function populateFighters() {
     fighters = [];
@@ -18,26 +16,36 @@ function populateFighters() {
     fighters[1] = new fighter("Space Marines", 150, 6, 15);
     fighters[2] = new fighter("Orks", 180, 5, 10);
     fighters[3] = new fighter("Chaos", 140, 7, 15);
+
+    clearRow("fgtSelect");
+    fighters.forEach(function (fighter, index) {
+        createCharCard(fighter, index, "fgtSelect");
+    });
 }
 function showFighters() {
-    clearRow("fgtSelect");
+    
     console.log("");
     console.log("<+-------= Current Fighters =-------+>");
-    fighters.forEach(function (fighter, index, fighters) {
-        console.log(fighters[index].toString());
-        createCharCard(fighters[index], index, "fgtSelect");
+    fighters.forEach(function (fighter) {
+        console.log(fighter.toString());
     });
     console.log("<+----------------------------------+>");
     console.log("");
 }
-function createCharCard(fighter, index, rowType) {
-    let fgtSelect = "#" + rowType + "-" + index;
-    $(fgtSelect).append(`<div class="card ${rowType}" id = ${fighter.name} style="width:175px"><div class="card-header">${fighter.name}</div><div class="card-body"><img class="img-responsive" src="Assets/images/${fighter.name}.jpg"alt="${fighter.name}"></div><div class="card-footer">HP: ${fighter.healthPoints}</div>`);
-}
-function clearRow(rowType) {
+function createCharCard(fighter, colNum, colType) {
+    
+    let fgtSelect;
+    if (colType === "inactive") {
+        fgtSelect = `#$enemy-${colNum}`
+    } else {
+        fgtSelect = `#${colType}-${colNum}`
+    }
 
+    $(fgtSelect).append(`<div class="card ${colType}" id = "${fighter.name}"><div class="card-header">${fighter.name}</div><div class="card-body"><img class="img-responsive" src="./Assets/images/${fighter.name}.jpg"alt="${fighter.name}"></div><div class="card-footer">HP: <span id = "${fighter.name}-HP">${fighter.healthPoints}</span></div>`);
+}
+function clearRow(colType) {
     for (let i = 0; i < 4; i++) {
-        let cardLoc = "#" + rowType + "-" + i;
+        let cardLoc = "#" + colType + "-" + i;
         $(cardLoc).html("");
     }
 }
@@ -45,30 +53,52 @@ function clearRow(rowType) {
 // 
 let fighters = [];
 let player;
+let playerLv = 1;
+let defender;
 console.log("Start game.js...");
 console.log("");
 populateFighters();
 showFighters();
 
-$(".fgtSelect").on("click", function() {
-    fighterName = $(this).attr("id");
-    fighters.forEach(function (fighter, index, fighters) {
-        if (fighters[index].name === fighterName) {
-            player = [fighters.splice(index, 1), 1];
-            
-            createCharCard(fighters[index], index, "playerCard");
-        }
+// $(document).ready(function () {
+    // Select a fighter.
+    $(".fgtSelect").on("click", function () {
+        let fighterName = $(this).attr("id");
+        
+        fighters.forEach(function (fighter, index) {
+            if (fighter.name === fighterName) {
+                let playerTmp = fighters.splice(index, 1);
+
+                player = playerTmp[0];
+                createCharCard(player, 1, "player");
+                showFighters();
+            }             
+        });
+        fighters.forEach(function (fighter,index) {
+            createCharCard(fighter, index, "enemy")
+        });
+        clearRow("fgtSelect");
     });
-    clearRow("fgtSelect");
+    // Select an enemy.
+    $(".enemy").on("click", function () {
+        console.log("enemy clicked");
+        let defenderName = $(this).attr("id");
+        console.log(`Defender Name: ${defenderName}`);
+        
+        fighters.forEach(function (fighter, index) {
+            if (fighter.name === defenderName) {
+                let defenderTmp = fighters.splice(index, 1);
+
+                defender = defenderTmp[0];
+                console.log(defender);
+                
+                createCharCard(defender, 1, "defender");
+                showFighters();
+            }             
+        });
+        clearRow("enemy");
+        fighters.forEach(function (fighter,index) {
+            createCharCard(fighter, index, "inactive")
+        });
+    });
 });
-
-// console.log(fighters.splice(2, 1).toString());
-
-// showFighters();
-
-// console.log(fighters.splice(2, 1).toString());
-
-// showFighters();
-
-// populateFighters();
-// showFighters();
